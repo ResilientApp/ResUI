69d9ad91792f9ca04e8c16509d06d17e0b73a9d2
Gopal Nambiar <gopalnambiar2@gmail.com>
2023-11-30 01:36:52 -0800

    Added sign up backend functionality

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
-// server.js
 const express = require('express');
+const bcrypt = require('bcrypt');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 const port = 5500;
 
-const uri = "mongodb+srv://username:password@cluster0.y7hp6fq.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://gopuman:Qwerty123@cluster0.y7hp6fq.mongodb.net/?retryWrites=true&w=majority";
 
 const client = new MongoClient(uri, {
     serverApi: {
@@ -16,6 +16,7 @@ const client = new MongoClient(uri, {
     }
   });
 
+app.use(express.json());
 app.use(cors());
 
 app.get('/api/instances', async (req, res) => {
@@ -31,6 +32,45 @@ app.get('/api/instances', async (req, res) => {
   } finally {
     await client.close();
   }
+});
+
+// Endpoint to handle user signup
+app.post('/api/signup', async (req, res) => {
+    try {
+      const { name, email, password } = req.body;
+
+      if (!name || !email || !password) {
+        return res.status(400).json({ error: 'Name, email, and password are required' });
+      }
+
+      // Connect to the database
+      await client.connect();
+      const database = client.db('resui');
+      const collection = database.collection('users');
+  
+      // Check if the user already exists
+      const existingUser = await collection.findOne({ email });
+      if (existingUser) {
+        return res.status(400).json({ error: 'User already exists' });
+      }
+  
+      // Hash the password before storing it
+      const hashedPassword = await bcrypt.hash(password, 10);
+  
+      // Insert the new user into the database
+      await collection.insertOne({
+        name,
+        email,
+        password: hashedPassword,
+      });
+  
+      res.status(201).json({ success: true, message: 'User registered successfully' });
+    } catch (error) {
+      console.error('Error during signup:', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    } finally {
+      await client.close();
+    }
 });
 
 app.listen(port, () => {
--- src/components/Signup/index.js
+++ src/components/Signup/index.js
@@ -1,37 +1,80 @@
-import React from 'react'
+import React, { useState } from 'react';
 import resilientDBLogo from '../../images/resilientdb_logo.svg';
 import { GlobalStyle, ImageContainer, LogoImage, SignUpForm, SignUpH2, SignUpInput, ButtonsContainer, SignUpButtons } from './SignUpElements';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const SignUp = () => {
+  // State variables
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  // Form submission handler
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+
+    // Validate password and confirm password
+    if (password !== confirmPassword) {
+      alert('Passwords do not match');
+      return;
+    }
+
+    // Send signup request to the server
+    try {
+      console.log(name, email, password);
+      const response = await fetch('http://localhost:5500/api/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          name: name,
+          email: email,
+          password: password,
+        }),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        alert(data.message);
+        // Redirect to login page or perform other actions
+      } else {
+        alert(`Error: ${data.error}`);
+      }
+    } catch (error) {
+      console.error('Error during signup:', error);
+      alert('Error during signup. Please try again.');
+    }
+  };
+
   return (
     <>
-      <GlobalStyle/>
+      <GlobalStyle />
       <ImageContainer>
         <Link to="/">
-          <LogoImage src={resilientDBLogo} width="300px" to="/"/>
+          <LogoImage src={resilientDBLogo} width="300px" alt="ResilientDB Logo" />
         </Link>
       </ImageContainer>
 
-      <SignUpForm>
+      <SignUpForm onSubmit={handleSignUp}>
         <SignUpH2>Sign Up</SignUpH2>
 
-        <SignUpInput type="text" placeholder="Name" />
-        <SignUpInput type="text" placeholder="Email" />
-        <SignUpInput type="password" placeholder="Password" />
-        <SignUpInput type="password" placeholder="Confirm Password" />
+        <SignUpInput type="text" placeholder="Name" onChange={(e) => setName(e.target.value)} />
+        <SignUpInput type="text" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+        <SignUpInput type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+        <SignUpInput type="password" placeholder="Confirm Password" onChange={(e) => setConfirmPassword(e.target.value)} />
 
         <ButtonsContainer>
           <SignUpButtons type="submit" active="yes">Sign Up</SignUpButtons>
           <Link to="/login">
-            <SignUpButtons type="submit" active="no">Log In</SignUpButtons>
+          <SignUpButtons type="button" active="no">Log In</SignUpButtons>
           </Link>
         </ButtonsContainer>
       </SignUpForm>
     </>
-  )
-}
-
-
+  );
+};
 
 export default SignUp;
