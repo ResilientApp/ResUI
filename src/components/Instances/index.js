d2b4d848cf028dcfd7c4cf721d532118cdfe97f8
Apratim Shukla <apratimshukla6@gmail.com>
2024-06-07 19:42:39 -0700

    Update index.js

--- src/components/Instances/index.js
+++ src/components/Instances/index.js
@@ -30,7 +30,7 @@ const Instances = () => {
         const token = localStorage.getItem("userToken");
 
         const response = await fetch(
-          `/api/instances?userEmail=${token}`,
+          `https://resui-backend.resilientdb.com/api/instances?userEmail=${token}`,
           {
             method: "GET",
             headers: {
